a288d3a3cbdd59e158777885daec8b2d81c2a22b
nebiyu-ethio <nebiyuethio@gmail.com>
2024-10-15 14:11:38 +0300

    add the static style

--- app/scoring/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import predict_credit_risk, home
-
-urlpatterns = [
-    path('api/predict/', predict_credit_risk, name='predict_credit_risk'),
-    path('', home, name='home'),
-]
